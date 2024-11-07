2c9c42e4128bc5877414cffb2f31d80eab072779
revalinaadelia <144590332+revalinaadelia@users.noreply.github.com>
2024-11-07 17:10:18 +0700

    App5.java

---
+++ App5.java
@@ -0,0 +1,7 @@
+public class App5 {
+    public static void main(String[] args) {
+        LibraryManager.addBooks(10);
+        LibraryManager.removeBooks(5);
+        LibraryManager.displayTotalBooks();
+    }
+}
