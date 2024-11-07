bd97362000e8ae6bb6455c7d27c9fa5ced5f7730
revalinaadelia <144590332+revalinaadelia@users.noreply.github.com>
2024-11-07 17:06:48 +0700

    Library.java

---
+++ Library.java
@@ -0,0 +1,8 @@
+public class Library {
+    static {
+        System.out.println("Library system initialized.");
+    }
+    public static String convertToUpperCase(String str) {
+        return str.toUpperCase();
+    }
+}
