de4e4bedb9aed61d7a245e41d75ba7336d349f83
revalinaadelia <144590332+revalinaadelia@users.noreply.github.com>
2024-11-07 17:08:21 +0700

    App3.java

---
+++ App3.java
@@ -0,0 +1,6 @@
+public class App3 {
+    public static void main(String[] args) {
+        User.Address address = new User.Address("New York", "NY", "10001");
+        address.displayAddress();
+    }
+}
